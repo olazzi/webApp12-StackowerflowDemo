5a35d4f86f9c2ea7db44f9f4202ac0ff52b7b7f5
olazzi <tarihenes@gmail.com>
2022-02-07 11:35:09 +0300

    Add project files.

---
+++ webApp12/Controllers/QuestionsController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using webApp12.Models;
+
+namespace webApp12.Controllers
+{
+    public class QuestionsController : Controller
+    {
+        private webappEntities db = new webappEntities();
+
+        // GET: Questions
+        public ActionResult Index()
+        {
+           
+            return View(db.Questions.ToList());
+
+        }
+        
+
+        //GET: Questions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+               return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+           }
+           Question question = db.Questions.Find(id);
+          if (question == null)
+            {
+              return HttpNotFound();
+           }
+            return View(question);
+
+        }
+
+        // GET: Questions/Create
+        public ActionResult Create()
+        {
+           
+            return View();
+        }
+
+        // POST: Questions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Question1,Answer,Id")] Question question)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Questions.Add(question);
+                db.SaveChanges();
+                TempData["SuccessMessage"] = "New Record Added Succesfully";
+                return RedirectToAction("Index");
+            }
+
+            return View(question);
+        }
+
+        // GET: Questions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Question question = db.Questions.Find(id);
+            if (question == null)
+            {
+                return HttpNotFound();
+            }
+            return View(question);
+        }
+
+        // POST: Questions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Question1,Answer,Id")] Question question)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(question).State = EntityState.Modified;
+                db.SaveChanges();
+                TempData["SuccessMessage"] = "New Record Editted Succesfully";
+                return RedirectToAction("Index");
+            }
+            return View(question);
+        }
+
+        // GET: Questions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Question question = db.Questions.Find(id);
+            if (question == null)
+            {
+                return HttpNotFound();
+            }
+            return View(question);
+        }
+
+        // POST: Questions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Question question = db.Questions.Find(id);
+            db.Questions.Remove(question);
+            db.SaveChanges();
+            TempData["SuccessMessage"] = "New Record Deleted Succesfully";
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ webApp12/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="webApp12.MvcApplication" Language="C#" %>
